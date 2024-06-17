d3d6d81381e1ad9c34db35198c3ce03d195c1d2f
CarlosAlvia <cjalvia@espol.edu.ec>
2024-06-16 19:12:59 -0500

    Tokens ID y BOOL

--- lexer.py
+++ lexer.py
@@ -4,8 +4,21 @@ import ply.lex as lex
 reserved = {"break": "BREAK", "default": "DEFAULT", "funct": "FUNCT", "Interface": "INTERFACE", "select": "SELECT", "case": "CASE", "defer": "DEFER", "Go": "GO", "map": "MAP", "struct": "STRUCT", "chan": "CHAN", "else": "ELSE", "Goto": "GOTO", "package": "PACKAGE", "switch": "SWITCH", "const": "CONST", "fallthrough": "FALLTHROUGH", "if": "IF", "range": "RANGE", "type": "TYPE", "continue": "CONTINUE", "for": "FOR", "import": "IMPORT", "return": "RETURN", "var": "VAR"}
 
 tokens = (
-    'BOOL',
+    'ID',
+    'BOOL'
 )+tuple(reserved.values())
 
 # Expresiones regulares
-t_BOOL = r'true|false'
+
+def t_ID(t):
+    r'[_a-zA-Z]\w*'
+    t.type = reserved.get(t.value,"ID")
+    return t
+
+def t_BOOL(t):
+    r'true|false'
+    if(t == 'true'):
+        t.value = True
+    else: 
+        t.value = False
+    return t
