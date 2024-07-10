86dc68b78319c6eff9c5fe10d26f1257d0256215
CarlosAlvia <cjalvia@espol.edu.ec>
2024-07-09 22:43:02 -0500

    Separación de algoritmos de prueba

---
+++ algoritmoAlvia.go
@@ -0,0 +1,15 @@
+func algoritmo(){
+
+    var entero1 int = (450  * 2 - 200 + 23) / (20%3)
+    var complejo1 complex64 = 2+12i
+    var verdadero bool = true
+    mensajes := [2]string{"Algoritmo", "Random"}
+    var flotante1 float64 = 23.92 
+
+    if v := 4.12; 3 + 3 > 10 {
+
+        flotante1 = flotante1 - v
+        complejo++
+
+    }
+}
--- algoritmoZarate.go
+++ algoritmoZarate.go
@@ -1,4 +1,3 @@
-// Algoritmo Sofía Zárate
 var complejo complex64 = 4.7+6.2i
 var arreglo = []int{10, 20, 30}
 var flotante float64 = 8.15
@@ -14,22 +13,5 @@ func main() {
         if flotante <= 5 {
             fmt.Println(cadena,entero)
         }
-    }
-}
-
-//Algoritmo Carlos Alvia
-func algoritmo(){
-
-    var entero1 int = (450  * 2 - 200 + 23) / (20%3)
-    var complejo1 complex64 = 2+12i
-    var verdadero bool = true
-    mensajes := [2]string{"Algoritmo", "Random"}
-    var flotante1 float64 = 23.92 
-
-    if v := 4.12; 3 + 3 > 10 {
-
-        flotante1 = flotante1 - v
-        complejo++
-
     }
 }
