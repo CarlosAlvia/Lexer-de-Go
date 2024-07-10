0d19d329638cf074d264d6154d0b2670fae26313
CarlosAlvia <cjalvia@espol.edu.ec>
2024-07-09 22:35:50 -0500

    Arreglo en el archivo de prueba

--- prueba.go
+++ prueba.go
@@ -1,8 +1,4 @@
-package main
-
-import "fmt"
-
-// Variables globales
+// Algoritmo Sofía Zárate
 var complejo complex64 = 4.7+6.2i
 var arreglo = []int{10, 20, 30}
 var flotante float64 = 8.15
@@ -12,13 +8,28 @@ func main() {
     if entero > 10 || flotante == 1.23 {
         var cadena string = "Hola Soledad"
         for i := 0; i < 2; i++ {
-            entero += i * int(flotante) / 2
+            entero = entero + 2
         }
 
         if flotante <= 5 {
-            fmt.Println(cadena)
-        } else {
-            fmt.Println(entero, complejo)
+            fmt.Println(cadena,entero)
         }
+    }
+}
+
+//Algoritmo Carlos Alvia
+func algoritmo(){
+
+    var entero1 int = (450  * 2 - 200 + 23) / (20%3)
+    var complejo1 complex64 = 2+12i
+    var verdadero bool = true
+    mensajes := [2]string{"Algoritmo", "Random"}
+    var flotante1 float64 = 23.92 
+
+    if v := 4.12; 3 + 3 > 10 {
+
+        flotante1 = flotante1 - v
+        complejo++
+
     }
 }
